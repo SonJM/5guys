eb613ad004a6c229af402e24b4e29fd54b5c17e6
Blisle <blisle@naver.com>
2025-07-20 23:15:44 +0900

    feat: 어플리케이션 다운로드 기능 추가(PWA)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+// @ts-check
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
+  // 프로젝트의 다른 Next.js 설정이 있다면 여기에 유지합니다.
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
